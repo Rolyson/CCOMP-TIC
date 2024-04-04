80ca8ce629349d00b914c89dd1175ad77c4ce20f
ROLYSON <97520994+Rolyson@users.noreply.github.com>
2024-04-04 13:48:19 -0300

    Ajustes na questão 3 e estrutura de pastas

---
+++ solucoes_exercicio_3/exercicio_3.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int solicitarNumero(){
+	int numero_digitado;
+	printf("Informe a quantidade de numeros que deseja armazenar: ");
+	scanf("%d", &numero_digitado);
+	
+	if(numero_digitado <= 0){
+		printf("Informe um numeo > 0 \n");
+		solicitarNumero();
+	}else{
+		return numero_digitado;
+	}
+}
+
+void ordenaArray(int array_length, int numeros[] ){
+	for (int i = 0; i < array_length; i++){
+        for (int j = i; j < array_length; j++) {
+            if (numeros[i] > numeros[j]) {
+                int temp = numeros[i];
+                numeros[i] = numeros[j];
+                numeros[j] = temp;
+            }
+        }
+    }
+}
+
+int main(){
+	int array_length, numero, aux;
+	
+	array_length = solicitarNumero();
+	
+	int numeros [array_length];
+	
+	for (int i = 0; i < array_length; i++){
+		printf("Digite o numero inteiro que deseja armazenar: ");
+		scanf("%d", &numero);
+
+		numeros[i] = numero;
+	}
+	
+	ordenaArray(array_length, numeros);
+	
+	for (int i = 0; i < array_length; i++){
+		printf("%d ",numeros[i]);	
+	} 
+
+	return 0;	
+}
