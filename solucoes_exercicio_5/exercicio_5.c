a2c51791f4963b264008f08c5f7f6a53defeddeb
ROLYSON <rolysonrocha@gmail.com>
2024-04-07 23:11:22 -0300

    Update exercicio_5.c

--- solucoes_exercicio_5/exercicio_5.c
+++ solucoes_exercicio_5/exercicio_5.c
@@ -25,8 +25,24 @@ void ordenaArray(int array_length, int numeros[] ){
     }
 }
 
+void ordenaDoisArray(int array_length, int numero_ocorrencia[], int numero_ordem[] ){
+	for (int i = 0; i < array_length; i++){
+        for (int j = i; j < array_length; j++) {
+            if (numero_ocorrencia[i] < numero_ocorrencia[j]) {
+                int temp_ocorrencia = numero_ocorrencia[i];
+                numero_ocorrencia[i] = numero_ocorrencia[j];
+                numero_ocorrencia[j] = temp_ocorrencia;
+                
+								int temp_numero = numero_ordem[i];
+                numero_ordem[i] = numero_ordem[j];
+                numero_ordem[j] = temp_numero;	
+            }
+        }
+    }
+}
+
 int main(){
-	int array_length, numero, numero_temp, ocorrencia;
+	int array_length, numero, numero_temp, ocorrencia, contador = 0;
 	
 	array_length = solicitarNumero();
 	
@@ -41,19 +57,38 @@ int main(){
 	
 	ordenaArray(array_length, numeros);
 
-	if(!numero_temp){
-		printf("Teste");	
-	}
 	
-	// for (int i = 0; i < array_length; i++){
-	// 	// if(numeros[i] != numero_temp){
-	// 	// 	numero_temp = numeros[i];
-	// 	// 	ocorrencia = 1;
-	// 	// } else if(numeros[i] == numero_temp){
-	// 	// 	ocorrencia ++
-	// 	// }
-		
-	// } 
+	int numero_ordem [array_length];
+	int numero_ocorrencia [array_length];
+
+	for (int i = 0; i < array_length; i++){
+		if(i == array_length){
+			numero_ordem[contador] = numeros[i];
+			numero_ocorrencia[contador] = ocorrencia;
+			contador ++;
+			
+		}else	if(numeros[i] != numero_temp){
+			if(i != 0){
+				numero_ordem[contador] = numero_temp;
+				numero_ocorrencia[contador] = ocorrencia;
+				contador ++;
+			}
+			numero_temp = numeros[i];
+			ocorrencia = 1;
+		} else if(numeros[i] == numero_temp){
+			ocorrencia ++;
+		}
+	} 
+
+	// for(int i = 0; i<contador; i++){
+	// 	printf("numero %d \n", numero_ordem[i]);
+	// }
+	
+	ordenaDoisArray(contador, numero_ocorrencia, numero_ordem);
+
+	for(int i = 0; i<contador; i++){
+		printf("numero %d : ocorrencia %d\n", numero_ordem[i], numero_ocorrencia[i]);
+	}
 
 	return 0;	
 }
