af3662bc5319266a875b4f618988257b0e30516d
ROLYSON <rolysonrocha@gmail.com>
2024-04-27 12:13:55 -0300

    Validação de numeros repetidos na aposta.

--- mega/main.c
+++ mega/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <time.h>
+#include <ctype.h>
 #include "megasena.h"
 
+
 int main() {
-    int opcao = 3, QD, QM;
+    int QD, QM;
+    char opcao = 1;
 
     do{
         QD = solicita_valor("Informe a quantidade de dezenas que deseja jogar na aposta (Valores validos de 6 a 15): ", 6, 15);
@@ -12,10 +15,13 @@ int main() {
         if(QM > 0){
             aposta_manual(QM, QD);
         }
-        
-    printf("fim");
-    }while(opcao <= 2);
 
+        printf("\nDeseja continuar (S/N)? \n");
+        scanf("%s", &opcao);
+        
+    }while(toupper(opcao) != 'N');
+
+    printf("Obrigado por participar das Loterias Caixa");
 
     
     return 0;
--- mega/megasena.c
+++ mega/megasena.c
@@ -7,20 +7,39 @@ int solicita_valor(char enunciado[], int vl_min, int vl_max){
     do{
         printf("%s", enunciado);
         scanf("%d", &variavel);
+        if(variavel < vl_min || variavel > vl_max){
+            printf("Valor invalido!\n");
+        }
     }while(variavel < vl_min || variavel > vl_max);
 
     return variavel;
 }
 
+int numeroRepetido(int numero, int array[], int tamanho){
+    for (int i = 0; i < tamanho; i++) {
+        if (array[i] == numero) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 void aposta_manual(int qm, int qd){
     int matriz[qm][qd];
-    char enunciado[] = "Informe o valor (Valores validos de 1 a 60): ";
-
+    printf("Informe o valor (Valores validos de 1 a 60):\n");
     for ( int i=0; i < qm; i++ ){
         printf("Aposta %d\n", i+1);
         for (int j=0; j < qd; j++ ){
-            // scanf ("%d", &matriz[ i ][ j ]);
-            matriz[ i ][ j ] = solicita_valor(enunciado, 1, 60);
+            int nrAposta, validado;
+            do{
+                nrAposta = solicita_valor("", 1, 60);
+                validado = numeroRepetido(nrAposta, matriz[i] ,qd);
+
+                if(validado){
+                    printf("Esse numero ja foi jogado na aposta\n");
+                }
+            }while(validado);
+            matriz[ i ][ j ] = nrAposta;
         }
     }
     
